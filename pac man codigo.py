4bc24d3a05eff0487044a57c6deb98e5a546f413
jronhaar <jronhaar@estudiantec.cr>
2023-11-04 11:07:07 -0600

    wasdddd

--- pac man codigo.py
+++ pac man codigo.py
@@ -13,6 +13,15 @@ fps = 60
 font = pygame.font.Font('freesansbold.ttf', 20)
 level = copy.deepcopy(boards)
 color = 'blue'
+PI = math.pi
+player_images = []
+for i in range (1, 5):
+    player_images.append(pygame.transform.scale(pygame.image.load(f'player_images/{i}.png'), (45, 45)))
+player_x = 450
+player_y = 663
+direction = 0
+counter = 0
+
 
 # codigo del cuadrado.
 def draw_board():
@@ -22,7 +31,7 @@ def draw_board():
         for j in range(len(level[i])):
             if level[i][j] == 1:
                 pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
-            if level[i][j] == 2 and not flicker:
+            if level[i][j] == 2:
                 pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
             if level[i][j] == 3:
                 pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1),
@@ -46,6 +55,39 @@ def draw_board():
             if level[i][j] == 9:
                 pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
                                  (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
+
+
+def draw_player():
+    # 0-RIGHT, 1-LEFT, 2-UP, 3-DOWN
+    if direction == 0:
+        screen.blit(player_images[counter // 5], (player_x, player_y))
+    elif direction == 1:
+        screen.blit(pygame.transform.flip(player_images[counter // 5], True, False), (player_x, player_y))
+    elif direction == 2:
+        screen.blit(pygame.transform.rotate(player_images[counter // 5], 90), (player_x, player_y))
+    elif direction == 3:
+        screen.blit(pygame.transform.rotate(player_images[counter // 5], 270), (player_x, player_y))
+
+
 run = True
+Paused = False
 while run:
     timer.tick(fps)
+    screen.fill('black')
+    draw_board()
+    draw_player()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    if pygame.key.get_pressed()[pygame.K_LSHIFT] or pygame.key.get_pressed()[pygame.K_RSHIFT]:
+        paused = True
+    else:
+        paused = False
+
+        # Update the game state only if the game is not paused
+    if not paused:
+        continue
+
+    pygame.display.flip()
+pygame.quit()
