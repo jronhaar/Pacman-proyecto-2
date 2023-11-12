de0606568ca3150bdae24ed429401fd3dfa148de
jronhaar <jronhaar@estudiantec.cr>
2023-11-11 20:09:22 -0600

    .py antes de agregar lo de Alejandro

--- pac man codigo.py
+++ pac man codigo.py
@@ -17,22 +17,70 @@ PI = math.pi
 player_images = []
 for i in range (1, 5):
     player_images.append(pygame.transform.scale(pygame.image.load(f'player_images/{i}.png'), (45, 45)))
-    blinky_img = pygame.transform.scale(pygame.image.load(f'ghost_images/red.png'), (45, 45))
-    pinky_img = pygame.transform.scale(pygame.image.load(f'ghost_images/pink.png'), (45, 45))
-    inky_img = pygame.transform.scale(pygame.image.load(f'ghost_images/blue.png'), (45, 45))
-    clyde_img = pygame.transform.scale(pygame.image.load(f'ghost_images/orange.png'), (45, 45))
-    spooked_img = pygame.transform.scale(pygame.image.load(f'ghost_images/powerup.png'), (45, 45))
+    rojo_img = pygame.transform.scale(pygame.image.load(f'ghost_images/red.png'), (45, 45))
+    pink_img = pygame.transform.scale(pygame.image.load(f'ghost_images/pink.png'), (45, 45))
+    azul_img = pygame.transform.scale(pygame.image.load(f'ghost_images/blue.png'), (45, 45))
+    naranja_img = pygame.transform.scale(pygame.image.load(f'ghost_images/orange.png'), (45, 45))
+    asustado_img = pygame.transform.scale(pygame.image.load(f'ghost_images/powerup.png'), (45, 45))
     dead_img = pygame.transform.scale(pygame.image.load(f'ghost_images/dead.png'), (45, 45))
 player_x = 450
 player_y = 663
 direction = 0
 direction_command = 0
+rojo_x = 56
+rojo_y = 58
+rojo_direction = 2
+pink_x = 440
+pink_y = 388
+pink_direction = 2
+azul_x = 440
+azul_y = 438
+azul_direction = 2
+naranja_x = 440
+naranja_y = 438
+naranja_direction = 2
 counter = 0
 valid_turns = [False, False, False, False] # R, L, UP, Down
 player_speed = 2
 score = 0
-def check_colision():
-    pass
+powerUp = False
+powerUp_count = 0
+ghosts_comidos = [False, False,False,False]
+targets = [(player_x, player_y), (player_x, player_y), (player_x, player_y), (player_x, player_y)]
+rojo_dead = False
+pink_dead = False
+azul_dead = False
+naranja_dead = False
+rojo_box = False
+pink_box = False
+azul_box = False
+naranja_box = False
+moving = False
+count_inicio = 0
+vida = 5
+def draw_texto():
+    score_text = font.render(f'Score: {score}',True, 'white') #True es para hacer el texto mas bonito
+    screen.blit(score_text,(30,485))
+    if powerUp:
+        pygame.draw.circle(screen,'red',(50, 550),15)
+    for i in range(vida):
+        screen.blit(pygame.transform.scale(player_images[0],(30, 30)), (650 + i * 40, 915)) # i * 40 hace que se mueva 40 pix a la derecha
+def check_colision(scor, powerUp, powerUp_count, ghosts_comidos):
+    num1 = (HEIGHT - 50) // 32
+    num2 = WIDTH // 30
+    if 0 < player_x < 870:
+        if level[center_y // num1][center_x // num2] == 1:
+            level[center_y // num1][center_x // num2] = 0
+            scor += 10
+        if level[center_y // num1][center_x // num2] == 2:
+            level[center_y // num1][center_x // num2] = 0
+            scor += 50
+            powerUp = True
+            powerUp_count = 0
+            ghosts_comidos = [False,False,False,False]
+
+    return scor, powerUp,powerUp_count,ghosts_comidos
+
 # codigo del cuadrado.
 def draw_board():
     num1 = ((HEIGHT - 50) // 32)
@@ -66,7 +114,6 @@ def draw_board():
                 pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
                                  (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
 
-
 def draw_player():
     # 0= derecha, 1 = izq, 2 = arriba, 3= abaj0
     if direction == 0:
@@ -77,7 +124,6 @@ def draw_player():
         screen.blit(pygame.transform.rotate(player_images[counter // 5], 90), (player_x, player_y))
     elif direction == 3:
         screen.blit(pygame.transform.rotate(player_images[counter // 5], -90), (player_x, player_y))
-
 
 def check_position(centerx, centery):
         turns = [False, False, False, False]
@@ -150,14 +196,29 @@ while run:
         counter += 1
     else:
         counter = 0
+
+    if powerUp and powerUp_count < 600: #600 es 10 sec a 60fps
+        powerUp_count += 1
+    elif powerUp and powerUp_count >= 600:
+        powerUp_count = 0
+        powerUp = False
+        ghosts_comidos = [False,False,False,False]
+    if count_inicio < 180: # 3s en 60fps
+        moving = False
+        count_inicio += 1
+    else:
+        moving = True
+
     screen.fill('black')
     draw_board()
     draw_player()
+    draw_texto()
     center_x = player_x + 23
     center_y = player_y + 24
     valid_turns = check_position(center_x, center_y)
-    player_x, player_y = move_player(player_x, player_y)
-    #score = check_colision(score)
+    if moving:
+        player_x, player_y = move_player(player_x, player_y)
+    score, powerUp, powerUp_count, ghosts_comidos = check_colision(score, powerUp, powerUp_count, ghosts_comidos)
 
 
     for event in pygame.event.get():
@@ -181,6 +242,7 @@ while run:
                 direction_command = direction
             if event.key == pygame.K_DOWN and direction_command == 3:
                 direction_command = direction
+
 
         for i in range(4):
             if direction_command == i and valid_turns[i]:
